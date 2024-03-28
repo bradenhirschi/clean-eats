a442f49c065e2f79a1314dcd1e70d9e22a43c5ef
baf20 <baf20@live.missouristate.edu>
2024-03-28 13:12:09 -0500

    Added allergies supabase funcitonality

--- app/screens/my-plan.tsx
+++ app/screens/my-plan.tsx
@@ -1,4 +1,4 @@
-import { Button, View, Text } from "react-native"
+import { Button, View, Text, Pressable, TextInput } from "react-native"
 import { supabase } from "../utils/supabase";
 import React, { useEffect, useState } from 'react';
 
@@ -10,59 +10,118 @@ interface Props {
 
 const MyPlanScreen = ({ navigation }: Props) => {
   const [userAllergies, setUserAllergies] = useState<string[]>([]);
-  const [id, setUserId] = useState<string[]>([]);
+  const [commonAllergies, setCommonAllergies] = useState<string[]>([]);
+  const [userId, setUserId] = useState<string>();
+  const [allergy, setAllergy] = useState('');
 
   useEffect(() => {
-    const getUserID = async() => {
-      try{
+    const fetchData = async () => {
+      try {
         const id = await supabase.auth.getUser().then((res) => res.data.user?.id)
-        console.log('User ID', id)
+        console.log('User ID', id);
+        setUserId(id);
+
+        const { data, error } = await supabase
+          .from('user_allergies')
+          .select('allergy')
+          .eq('user_id', id)
+
+        if (error) {
+          console.error('Error fetching data:', error.message);
+        } else {
+          console.log('Data fetched successfully:', data);
+          const allergies = data.map((d) => d.allergy);
+          setUserAllergies(allergies);
+        }
+      } catch (error) {
+        console.error('Error:');
       }
-    catch (error){
-      console.log('error');
-    }
-};
-getUserID();
-}, [])
+
+    };
+    fetchData();
+  }, [])
+
+  // useEffect(() => {
+  //   const getUserAllergies = async () => {
+
+  //   };
+  //   getUserAllergies();
+  // }, []);
 
   useEffect(() => {
-    const getUserAllergies= async() => {
-    try{
+    const getCommonAllergies = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('common_allergies').select('Allergy')
+        if (error) {
+          console.error('Error fetching data:', error.message);
+        } else {
+          console.log('Data fetched successfully:', data);
+          const allergies = data.map((d) => d.Allergy);
+          setCommonAllergies(allergies);
+        }
+      } catch (error) {
+        console.error('Error with get common allergies');
+      }
+    };
+    getCommonAllergies();
+  }, []);
+
+  const createUserAllergies = async (allergyInput?: string) => {
+
+    let i = allergyInput ? allergyInput : allergy.toLowerCase();
+
+    try {
       const { data, error } = await supabase
-    .from('user_allergies')
-    .select('allergy').match(id)
-    // .eq({user_id: id}) //user...allergies
-    // const id = await supabase.auth.getUser().then((res) => res.data.user?.id)
-    if (error){
-      console.error('Error fetching data:', error.message);
-    }else{
-      console.log('Data fetched successfully:', data);
-      console.log('User ID', id)
-      const allergies = data.map((d) => d.allergy);
-      setUserAllergies(allergies);
+        .from('user_allergies')
+        .insert({ user_id: userId, allergy: i })
+
+      if (error) {
+        console.log(error)
+      }
+      else {
+        console.log("Data inserted successfully.")
+        setAllergy('');
+      }
+
+    } catch (error) {
+      console.error(error)
     }
-    }catch (error) {
-      console.error('Error:');
-    }
-  };
-  getUserAllergies();
-}, []);
-  
+  }
+
+
 
   return (
     <View className="flex-1 items-center justify-center">
-    <View className="flex-1 items-center justify-center">
-          {userAllergies?.map((allergies) => (
-            <View
-              key={allergies}
-              className="p-2"
-            >
-              <Text className="textpbase">{allergies}</Text>
-            </View>
-          ))}
+      <View className="flex-1 flex flex-row items-center justify-center">
+        {commonAllergies?.map((Allergy) => (
+          <View
+            key={Allergy}
+          >
+            <Pressable className="bg-[#5e9e38] rounded-lg px-4 py-2"
+              onPress={() => createUserAllergies(Allergy)}>
+              <Text className="text-white">{Allergy}</Text>
+            </Pressable>
           </View>
+        ))}
+        {/* {commonAllergies.map((allergy) => (
+          <Button>{allergy}</Button>
+    ))} */}
+        <View>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingHorizontal: 10 }}
+            placeholder="Enter new allergy"
+            value={allergy}
+            onChangeText={text => setAllergy(text)}
+          />
+          <Pressable
+            className="bg-[#5e9e38] rounded-lg px-4 py-2"
+            onPress={() => createUserAllergies()}>
+            <Text className="text-white">Create New Allergy</Text>
+          </Pressable>
+        </View>
       </View>
-
+    </View>
   );
 }
 
